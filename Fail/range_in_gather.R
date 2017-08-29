0407ab6133706f98ecbdbc66814c6d094e541e67
JCSzamosi <jc.szamosi@alumni.utoronto.ca>
2017-08-29 15:02:29 -0400

    tidy and document

--- Fail/range_in_gather.R
+++ Fail/range_in_gather.R
@@ -1,6 +1,9 @@
-# Make the data set
+# tidyr version 0.7.0 has broken programmatic specification of column names.
+# This is my MRE for the issue. For now I have solved it by dowgrading to 0.6.1.
 
 library(tidyr)
+
+# Make the data set
 dat = data.frame(Genus = 'Escherichia',
 					SP1 = rep(10,10),
 					SP2 = rep(5,10),
@@ -10,12 +13,10 @@ dat = data.frame(Genus = 'Escherichia',
 dat %>% gather(Species,Count,2:4) -> dat_long
 
 # Specify columns more programmatically. This used to work but now doesn't.
-
 st = 2
 dat %>% gather(Species,Count,st:ncol(dat)) -> dat_long
 
 # This also fails, but more excitingly:
-
 st = 1
 dat %>% gather(Species,Count,(st+1):ncol(dat)) -> dat_long
 
